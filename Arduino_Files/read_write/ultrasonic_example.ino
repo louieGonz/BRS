757eeab7fdd93da42e7bda52ad654c12ff42abb4
jpdef <jacobdefilippis@gmail.com>
2015-02-03 15:42:41 -0800

    comm iter 5, steady flow of data

--- Arduino_Files/read_write/ultrasonic_example.ino
+++ Arduino_Files/read_write/ultrasonic_example.ino
@@ -2,19 +2,83 @@
  
  //FOR SESNORS CANNOT BE RUN CONCURRENTLY
 
+struct data_buffer;
+struct node;
+struct data_buffer* data;
 const int trigPin_1 = 2;
 const int trigPin_2 =12;
 const int echoPin_1 = 4;
 const int echoPin_2 = 8; 
-const int packet_size = 10;
+const int packet_size = 16;
 const byte err_packet[] = {0xEF};
 int startByte =0;
+
+//LIFO
+struct data_buffer{
+
+     struct node* head;
+     struct node* tail;
+     int size;
+};
+
+struct node{
+    struct node* next;
+    long r;
+};
+
+
+void  data_buffer(){
+      data  = (struct data_buffer*) malloc(sizeof( struct data_buffer));
+      data->head=NULL;
+      data->tail=NULL;
+      int size =0;
+  
+}
+
+void push(long r){
+     struct node* n =(struct node*)malloc(sizeof( struct node));
+     n->r = r;
+     if(data->size == 0){
+        n->next =NULL;
+        data->head = n;
+        data->tail = n;
+        ++(data->size);
+     }else{
+       n->next = data->head;
+       data->head = n;
+       ++(data->size);
+     }
+        
+}
+
+long pop(){
+    long temp = 0;
+    if(data->tail == NULL){
+          return temp;
+     }else{
+         temp = data->tail->r;
+     } 
+
+    if(data->size ==1){
+          data->tail = data->head = NULL;
+          data->size =0; 
+    }else{
+     struct node* curr =data->head;
+        for(int i=0;i<data->size;++i){
+           if(curr->next = data->tail){break;}
+           if(curr->next =NULL){ return -1;}
+              curr = curr->next;
+         }
+      data->tail = curr;
+    }
+    return temp;
+}
 
 void setup() {
   // initialize serial communication:
   Serial.begin(115200);
-  startByte = Serial.read();
-
+  srand(42);
+  data_buffer();
 }
  
 long microsecondsToInches(long microseconds)
@@ -30,16 +94,16 @@ long microsecondsToInches(long microseconds)
 void long_to_byte(long num, byte* byte_array, int start){
      //Manipulates byte array, start contains lowest order byte
      //Shift back and add to obtain original data
+     //Probably only need 2byte representation
 
      byte_array[start] = (num & 0x000000FFUL);
      byte_array[start+1] = (num & 0x0000FF00UL) >> 4;
-     byte_array[start+2] = (num & 0x00FF0000UL) >> 16;
-     byte_array[start+3] = (num & 0xFF000000UL) >> 24;
+     //byte_array[start+2] = (num & 0x00FF0000UL) >> 16;
+     //byte_array[start+3] = (num & 0xFF000000UL) >> 24;
 }
 
 
 long get_distance(int trig_pin, int echo_pin){
-
       //Transmit
       pinMode(trig_pin, OUTPUT);
       digitalWrite(trig_pin, LOW);
@@ -61,28 +125,41 @@ long get_distance(int trig_pin, int echo_pin){
 
 
 void send_packet(long r_1, long r_2,int err){
-     /* Need to implement error packet*/    
- 
+     /* Need to implement error packet*/     
      byte* packet = (byte*)malloc(packet_size*sizeof(byte));
-     packet[0] = 0xFF;                // start 
-     long_to_byte(r_1,packet,1);      // data
-     long_to_byte(r_2,packet,5);
-     packet[packet_size-1] =  0xEE;  //end
+     packet[0] = 0x00; 
+     packet[1] = 0xFF;               // start 
+     long_to_byte(r_1,packet,2);      // data
+     long_to_byte(r_2,packet,4);
+     long_to_byte(r_2,packet,6);
+     long_to_byte(r_2,packet,8);
+     long_to_byte(r_2,packet,10);
+     long_to_byte(r_2,packet,12);
+     packet[packet_size-2] =  0xEE; //end
+     packet[packet_size-1] =  0x00;  
            
      Serial.write(packet,packet_size);
 
 }
 
+void serialEvent(){
+/*      int sigByte = Serial.read();
+      if(sigByte == (byte) 0xFF){
+        
+        delay(500);
+        send_packet(rand(),rand(),1);      
+      }else{
+         send_packet(0,0,1);
+      } 
+*/
+}
+
 void loop()
 {
-   if(Serial.available()>0){
-      int startByte = Serial.read();
-      if(startByte == (byte) 0xFF){
-         long r1 =  get_distance(trigPin_1, echoPin_1);
-         long r2 =  get_distance(trigPin_2, echoPin_2);
-         send_packet(r1,r2,1);
-      } 
-   } 
+  //push(get_distance(trigPin_1,echoPin_1));     
+  //push(get_distance(trigPin_2,echoPin_2));    `
+   delay(1500);
+   send_packet(rand(),rand(),1);      
 }    
      
      
--- app/src/main/java/brs/com/brs/DeviceDetect.java
+++ app/src/main/java/brs/com/brs/DeviceDetect.java
@@ -7,6 +7,7 @@ import android.hardware.usb.UsbManager;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.os.Handler;
 import android.widget.TextView;
@@ -29,12 +30,20 @@ public class DeviceDetect extends Activity {
     private UsbManager mUsbManager;
     private UsbSerialPort mPort;
     private UsbDeviceConnection mConnection;
+    private TextView infoView;
     private int packetSize =10;
     private final byte sig_start = (byte) 0xFF;
     private final byte sig_error = (byte) 0xEF;
     private final byte sig_kill = (byte)  0xEE;
 
-    protected List<UsbSerialPort> get_ports(){
+
+/*-----------MAIN  FN'S--------------*/
+    /*
+      getPorts()
+            -gets manager for driver
+            -driver gets list of ports
+    */
+    protected List<UsbSerialPort> getPorts(){
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         List<UsbSerialDriver> mDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(mUsbManager);
         List<UsbSerialPort> ports = null;
@@ -50,6 +59,10 @@ public class DeviceDetect extends Activity {
 
     }
 
+    /*
+      connectToDevice()
+            -opens connection to port
+    */
     protected void connectToDevice(UsbSerialPort port){
         UsbDeviceConnection mConnection = mUsbManager.openDevice(port.getDriver().getDevice());
         try{
@@ -67,7 +80,7 @@ public class DeviceDetect extends Activity {
         //Read from port
         try {
             byte buffer_in[] = new byte[packetSize];
-            int numBytesRead = mPort.read(buffer_in, 2000);
+            int numBytesRead = mPort.read(buffer_in, 500);
 
             //Check if buffer has pertinent data
             //Need better way to capture data
@@ -97,34 +110,52 @@ public class DeviceDetect extends Activity {
         byte[] sig_out = new byte[1];
         sig_out[0] = sig;
         try{
-            mPort.write(sig_out,1000);
-            sucess_message("Wrote to port");
+            mPort.write(sig_out,500);
+
         }catch (IOException e4){
             failure_message("Couldn't write");
+
             return;
         }
 
     }
 
-
+    protected String get(byte sig){
+        try {
+            byte buffer_in[] = new byte[32];
+            int numBytesRead =0;
+            int num = mPort.read(buffer_in, 200);
+            String message = "Bytes:" + HexDump.dumpHexString((buffer_in)) + "\n";
+            return message;
+           } catch (IOException e6) {
+            failure_message("Couldn't Read/Write");
+            try {
+                mPort.close();
+            } catch (IOException e7) {
+                //ignore
+            }
+        }
+         return "nothing";
+    }
 
     /*
-            Runnable for timed read input
+       Runnable for timed communication with arduino
     */
     final Handler readWriteHandle = new Handler();
     final Runnable readWriteRun = new Runnable(){
         @Override
         public void run(){
-            writePort(sig_start);
-            readPort();
+            infoView.append(get(sig_start));
+            infoView.setMovementMethod(new ScrollingMovementMethod());
             /*This is when the program is set
               to be called again*/
-            readWriteHandle.postDelayed(this,1000);
+            readWriteHandle.postDelayed(this,500);
             //on close write sig_stop
 
         }
     };
 
+/*--------- TEST INTERFACE ---------*/
 
 
 
@@ -132,6 +163,9 @@ public class DeviceDetect extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.device_detect);
+        infoView =(TextView)findViewById(R.id.infoMessage);
+        infoView.setText("Information:\n");
+        infoView.setTextSize(20);
 
         /*Setup Connection
          *   -Find Drivers
@@ -139,14 +173,13 @@ public class DeviceDetect extends Activity {
          *   -Connect to a port
          */
 
-        if(get_ports() != null){
-            mPort = get_ports().get(0);
+        if(getPorts() != null){
+            mPort = getPorts().get(0);
         }else{
             failure_message("Couldn't get a port");
             return;
         }
         connectToDevice(mPort);
-
 
 
 
@@ -168,8 +201,8 @@ public class DeviceDetect extends Activity {
 
         //If no port connect to port
         if (mPort == null) {
-            if (get_ports() != null) {
-                mPort = get_ports().get(0);
+            if (getPorts() != null) {
+                mPort = getPorts().get(0);
                 connectToDevice(mPort);
 
             } else {
@@ -204,20 +237,19 @@ public class DeviceDetect extends Activity {
         // The activity is about to be destroyed.
     }
 
-
+/*-----------UTIL FN'S-------------*/
 
     public void failure_message(String warn){
-        TextView textView = new TextView(this);
-        textView.setText("Failure" + warn);
-        textView.setTextSize(40);
-        setContentView(textView);
+        TextView failureView = (TextView)findViewById(R.id.failureMessage);
+        failureView.setText("Failure" + warn);
+        failureView.setTextSize(40);
     }
 
     public void sucess_message(String warn){
-        TextView textView = new TextView(this);
-        textView.setText("Sucess" + warn);
-        textView.setTextSize(40);
-        setContentView(textView);
+        TextView successView = (TextView)findViewById(R.id.sucessMessage);
+        successView.append("Sucess" + warn);
+        successView.setTextSize(40);
     }
+
 
 }
